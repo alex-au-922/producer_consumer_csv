e31dee1749fc790ad69f15d656c31635981bd451
alexau <alexuau922@gmail.com>
2023-12-02 22:45:36 +0800

    Added the init db test script

--- consumer/tests/test_adapters/test_upsert_iot_records/test_postgres/conftest.py
+++ consumer/tests/test_adapters/test_upsert_iot_records/test_postgres/conftest.py
@@ -4,6 +4,32 @@ import psycopg2
 import pytest
 
 
+@pytest.fixture(scope="session", autouse=True)
+def init_postgres_tables() -> None:
+    with psycopg2.connect(
+        host=PostgresConfig.HOST,
+        port=PostgresConfig.PORT,
+        user=PostgresConfig.USERNAME,
+        password=PostgresConfig.PASSWORD,
+        database=PostgresConfig.DATABASE,
+    ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS records (
+                    record_time TIMESTAMPTZ NOT NULL,
+                    sensor_id CHAR(64) NOT NULL,
+                    value DOUBLE PRECISION NOT NULL,
+                    PRIMARY KEY(record_time, sensor_id)
+                );
+
+                CREATE INDEX IF NOT EXISTS idx_records_record_time ON records USING BTREE (record_time);
+                CREATE INDEX IF NOT EXISTS idx_records_sensor_id ON records USING BTREE (sensor_id);
+                """
+            )
+            conn.commit()
+
+
 @pytest.fixture(scope="function")
 def postgres_upsert_iot_records_client() -> PostgresUpsertIOTRecordsClient:
     return PostgresUpsertIOTRecordsClient(
